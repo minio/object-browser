a7dd0954efecfe1cc820ab7a1b578c60ef4e3294
Nicolas <nicolasleblanc27@gmail.com>
2024-04-03 16:16:16 -0400

    add bucket level config

---
+++ portal-ui/src/screens/Console/Buckets/ListBuckets/AddBucket/AddBucketS3Config.tsx
@@ -0,0 +1,66 @@
+// This file is part of MinIO Console Server
+// Copyright (c) 2022 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import React from "react";
+import { useSelector } from "react-redux";
+import { AppState, useAppDispatch } from "../../../../../store";
+import { useTranslation } from 'react-i18next';
+import { Button, styled } from "@mui/material";
+
+
+const AddBucketS3Config = () => {
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+
+  return (
+    <Button
+      component="label"
+      role={undefined}
+      variant="contained"
+      tabIndex={-1}
+    >
+      Upload s3 config file
+      <VisuallyHiddenInput type="file" id="fileInput" onChange={async (event) => {
+        if (event?.target.files !== null) {
+
+          var aa = document.getElementById('fileInput') as any;
+          if (aa !== null) {
+            console.log(aa.files[0])
+            var reader = new FileReader()
+            reader.onload = async (e) => {
+              console.log(e.target?.result)
+            }
+            reader.readAsText(aa.files[0])
+          }
+        }
+      }} />
+    </Button>
+
+
+
+  );
+};
+
+export default AddBucketS3Config;
